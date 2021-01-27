eea4c56b7fe0cd3de9b2f34bcc3bfe4b4a248316
danieleperse <danieleperse3@gmail.com>
2021-01-27 15:00:02 -0300

    correcao lint

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,8 +1,8 @@
 db.movies.aggregate([
   { $match: {
     "imdb.rating": { $gte: 7 },
-    genres: { $nin: [ "Crime", "Horror" ] },
-    rated: { $in: [ "PG", "G" ] },
-    $and: [ { languages: { $eq: "English" } }, { languages: { $eq: "Spanish" } }]
-  }}
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    $and: [{ languages: { $eq: "English" } }, { languages: { $eq: "Spanish" } }],
+  } },
 ]);
