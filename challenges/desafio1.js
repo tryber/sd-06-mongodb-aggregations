a3f93aac253466b16c833eaf8347e8b1d13c54eb
pa-volpin <paulo.volpin@gmail.com>
2021-01-27 15:04:22 -0300

    Starting project :rocket: | Task 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,6 @@
+db.movies.aggregate([
+  { $match: { "imdb.rating": { $gte: 7} } },
+  { $match: { $nor: [{ genres: "Crime"}, {genres: "Horror"}] } },
+  { $match: { $or: [{ rated: "PG"}, {rated: "G"}] } },
+  { $match: { $and: [{ languages: "English"}, {languages: "Spanish"}] } },
+]);
