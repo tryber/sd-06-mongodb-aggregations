6e75f9a919ebc97258aeca116f4245a54bd45a90
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-27 17:15:38 -0300

    feat: project names with filter

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -7,4 +7,14 @@ db.movies.aggregate([
       languages: { $all: ["English", "Spanish"] },
     },
   },
+  {
+    $project: {
+      _id: false,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
 ]);
