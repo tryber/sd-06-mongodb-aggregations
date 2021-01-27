53c35115235423201194599714efbdd5bb931aa0
Júlia Pôrto <juportob@gmail.com>
2021-01-27 15:00:36 -0300

    Desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,14 +1,10 @@
-db.movies
-  .aggregate([
-    {
-      $match: {
-        $and: [
-          { "imdb.rating": { $gte: NumberDecimal(7) } },
-          { genres: { $ne: ["Crime", "Horror"] } },
-          { rated: { $in: ["PG", "G"] } },
-          { languages: { $in: ["English", "Spanish"] } },
-        ],
-      },
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
     },
-  ])
-  .pretty();
+  },
+]);
