b069cc755555bbf6c2caaa76da684d44c8413865
Isa Koga <isadorakoga710@gmail.com>
2021-01-27 15:37:05 -0300

    :memo: Challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,11 @@
+db.movies.aggregate([
+  { $project: {
+    _id: 0,
+    title_split: {
+      $split: ["$title", ""],
+    }
+  } },
+  { $match: {
+    $expr: { $eq: [{ $size: "$title_split"}, 1] },
+  } },
+]);
