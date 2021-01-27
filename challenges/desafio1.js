2e546218aa61fe25acf1346aeeb52ca18b497c81
William <williamjog@hotmail.com>
2021-01-27 15:58:15 -0300

    1º desafio - faltou o rated

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,6 +1,7 @@
 db.movies.aggregate([
   { $match: { "imdb.rating": { $gte: 7 } } },
   { $match: { genres: { $nin: ["Crime", "Horror"] } } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
   { $match: { languages: { $all: ["English", "Spanish"] } } },
   { $limit: 41 },
 ]);
