3098bff8a77bf21166fcf0c56f413b6235f917e3
HCaetano <hrcgeo@gmail.com>
2021-01-27 15:12:05 -0300

    Add challenges 1, 2 and 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $not: { $in: ["Crime", "Horror"] } },
+      rated: { $in: ["PG", "G"] },
+      languages: { $in: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      título: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    }
+  },
+  {
+    $sort: { notaIMDB: -1, título: 1 }
+  }
+]);
