ebf5711c22f4366ce6a4eaf26d36ab61fef06298
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-27 17:23:19 -0300

    Desafio6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,7 +1,7 @@
 db.movies.aggregate([
   { $match: { awards: { $regex: /won \d oscar/i } } },
   { $group: {
-    id: null,
+    _id: null,
     maior_rating: { $max: "$imdb.rating" },
     menor_rating: { $min: "$imdb.rating" },
     media_rating: { $avg: "$imdb.rating" },
@@ -11,6 +11,5 @@ db.movies.aggregate([
     maior_rating: 1,
     menor_rating: 1,
     media_rating: { $round: ["$media_rating", 1] },
-    desvio_padrao: { $round: ["$desvio_padrao", 1] },
-  } },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] } } },
 ]);
