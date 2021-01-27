a69f8f869fb5727153415f5b34d5b548406fefff
Thales Rangel <thalesgr@gmail.com>
2021-01-27 16:12:12 -0300

    ate o 5

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.movies.aggregate(
+  [
+    {
+      $project: {
+        _id: 0,
+        title_split: { $split: ["$title", " "] },
+      },
+    },
+    {
+      $match: {
+        $expr: { $eq: [{ $size: "title_split" }, 1] },
+      },
+    },
+  ],
+);
