e93bf32b62f396652cbdc9abb5337b167198a0e1
Thales Rangel <thalesgr@gmail.com>
2021-01-27 18:40:41 -0300

    desafio 10 correcao

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,22 +1,20 @@
-db.trips.aggregate(
-  [
-    {
-      $group:
-      {
-        _id: "$usertype",
-        duracaoMedia: {
-          $avg: {
-            $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
-          },
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
         },
       },
     },
-    {
-      $project: {
-        _id: 0,
-        tipo: "$usertype",
-        duracaoMedia: 1,
-      },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
     },
-  ],
-);
+  },
+  { $sort: { duracaoMedia: 1 } },
+]);
