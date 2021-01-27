df5c4dfa807bc6e624f6987729fa8c89c05dcf06
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-01-27 17:17:24 -0300

    Subindo desafio 5 e 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,22 +1,27 @@
-// const atoresFavoritos = [
-//   "Sandra Bullock",
-//   "Tom Hanks",
-//   "Julia Roberts",
-//   "Kevin Spacey",
-//   "George Clooney",
-// ];
+const atoresFavoritos = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
 
 db.movies.aggregate([
   {
     $match: {
-      // preciso dar um jeito de por este trem numa constante.
-      cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
+      cast: { $in: atoresFavoritos },
       countries: { $all: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
     },
-    // Takes two or more arrays and returns an array that contains
-    // the elements that appear in every input array.
-    // { $setIntersection: [ <array1>, <array2>, ... ] }
-    // $addFields: { num_favs: { $size: { $setIntersection: [atoresFavoritos, "$cast"] } } }
+  },
+  // $setIntersection => Takes two or more arrays and returns an array that contains
+  // the elements that appear in every input array.
+  // { $size: <expression> }
+  // colocar dentro do $size { $setIntersection: [ <array1>, <array2>, ... ] }
+  { $addFields: { num_favs: { $size:
+    { $setIntersection:
+      [atoresFavoritos, "$cast"] } } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $project: { _id: 0, title: 1 },
   },
 ]);
