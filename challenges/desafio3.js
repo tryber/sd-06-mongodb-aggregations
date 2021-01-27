57a4c71eafe45c0e7de32058167c3ab4e726cc00
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-01-27 19:00:24 -0300

    Desafio 3 feito

---
+++ challenges/desafio3.js
@@ -0,0 +1,21 @@
+db.movies.aggregate(
+  [{ $match: {
+    $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { $nor: [
+        { genres: { $elemMatch: { $eq: "Crime" } } },
+        { genres: { $elemMatch: { $eq: "Horror" } } },
+      ] },
+      { $or: [{ rated: "PG" }, { rated: "G" }] },
+      { $and: [{ languages: "English" }, { languages: "Spanish" }] },
+    ],
+  } },
+  { $project: {
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+    _id: 0 } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } }],
+);
