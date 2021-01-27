2412ea6c451dc5c018c31e0127c065f4a80b7cf8
phellipe5k <luizinhophe@hotmail.com>
2021-01-27 19:01:53 -0300

    challenge 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,1 @@
-db.movies.aggregate([{ $project: { _id: 0, title_split: { $split: ["$title", " "] } } }, { $match: { "title_split.1": { $exists: false } } }, { $sort: { title: 1 } }]);
+db.movies.aggregate([{ $sort: { title: 1 } }, { $project: { _id: 0, title_split: { $split: ["$title", " "] } } }, { $match: { title_split: { $size: 1 } } }]);
