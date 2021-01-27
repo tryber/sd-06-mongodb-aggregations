e2d2066d367524fc8e760e7d5f443c3ac9f9351d
Thales Rangel <thalesgr@gmail.com>
2021-01-27 16:44:24 -0300

    correcao 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -2,16 +2,25 @@ db.movies.aggregate(
   [
     {
       $match: {
-        awards: { $exists: true},
+        awards: { $regex: /won \d oscar/i },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        maior_rating: { $max: "$imdb.rating" },
+        menor_rating: { $min: "$imdb.rating" },
+        media_rating: { $avg: "$imdb.rating" },
+        desvio_padrao: { $stdDevSamp: "$imdb.rating" },
       },
     },
     {
       $project: {
         _id: 0,
-        maior_rating: { $max: "$imdb.rating" },
-        menor_rating: { $min: "$imdb.rating" },
-        media_rating: { $round: [{ $avg: "$imdb.rating" }] },
-        desvio_padrao: { $round: [{ $stdDevSamp: "$imdb.rating" }] },
+        maior_rating: 1,
+        menor_rating: 1,
+        media_rating: { $round: ["$media_rating", 1] },
+        desvio_padrao: { $round: ["$desvio_padrao", 1] },
       },
     },
   ],
