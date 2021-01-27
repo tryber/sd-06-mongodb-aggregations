27ae501994a818a95cbd8b2833c2ad962a13d588
FlavioSugano <flavio.sugano@gmail.com>
2021-01-27 19:07:06 -0300

    Desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: { languages: "English" },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $addFields: {
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  {
+    $sort: { numeroFilmes: -1 },
+  },
+]);
