a2547818c1423b30682ae9d3fac2c8d16a0db5bb
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-01-27 18:13:33 -0300

    refatorando a filtragem do genres do desafio 1 consequentemente refatorando o 2 e 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
   {
     $match: {
       "imdb.rating": { $gte: 7 },
-      $or: [{ genres: { $not: { $all: ["Crime"] } } }, { genres: { $not: { $all: ["Horror"] } } }],
+      genres: { $nin: ["Crime", "Horror"] },
       rated: { $in: ["PG", "G"] },
       languages: { $all: ["English", "Spanish"] },
     },
