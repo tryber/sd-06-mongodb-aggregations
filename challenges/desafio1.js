b181f875f67bc54d315db2ffbfb301b83fd0c3ff
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-27 15:57:50 -0300

    Solved wrong genres matching in challenges 1 and 2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,7 +3,7 @@ db.movies.aggregate([
     $match: {
       $and: [
         { "imdb.rating": { $gte: 7 } },
-        { genres: { $ne: { $all: ["Crime", "Horror"] } } },
+        { genres: { $nin: ["Crime", "Horror"] } },
         { $or: [{ rated: "PG" }, { rated: "G" }] },
         { languages: { $all: ["English", "Spanish"] } },
       ],
