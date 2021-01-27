a507990780fd888d38eb5663da96c1cd99f3fce3
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-27 19:38:51 -0300

    Solved challenge 4, ambiguous readme

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -19,11 +19,6 @@ db.movies.aggregate([
   {
     $project: {
       _id: 0,
-      titulo: "$title",
-      avaliado: "$rated",
-      notaIMDB: "$imdb.rating",
-      votosIMDB: "$imdb.votes",
-      ano: "$year",
       title_split: 1,
     },
   },
