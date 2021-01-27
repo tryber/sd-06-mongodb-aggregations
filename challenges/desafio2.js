5ec9219d3723b431d754ca94f045fd19aeac0d49
William <williamjog@hotmail.com>
2021-01-27 16:31:12 -0300

    2º Desafio

---
+++ challenges/desafio2.js
@@ -0,0 +1,15 @@
+db.movies.aggregate([
+  { $match: { "imdb.rating": { $gte: 7 } } },
+  { $match: { genres: { $nin: ["Crime", "Horror"] } } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
+  { $match: { languages: { $all: ["English", "Spanish"] } } },
+  { $limit: 41 },
+  { $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+  } },
+]);
