a1466f75306da53fe0f4f0860695286dd9ba4951
RenatoCostaDev <renatotrybe@gmail.com>
2021-01-27 17:21:09 -0300

    fix:Requirement-1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,5 +1,5 @@
 db.movies.aggregate([
-  { $match: { $and: [{ "imdb.rating": { $lt: 7 } }, { genres: { $nin: ["Crime", "Horror"] } },
+  { $match: { $and: [{ "imdb.rating": { $gte: 7 } }, { $and: [{ genres: { $ne: "Crime" } }, { genres: { $ne: "Horror" } }] },
     { $or: [{ rated: "PG" }, { rated: "G" }] }, { languages: { $all: ["English", "Spanish"] } },
   ] },
   },
