84f9601e073615efad13f55d7478191efee82828
deko possas <andrepossaster@gmail.com>
2021-01-27 18:21:08 -0300

    correcao do segundo

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,20 +1,22 @@
-db.movies.aggregate([
-  {
-    $match: {
-      "imdb.rating": { $gte: 7 },
-      genres: { $nin: ["Horror", "Crime"] },
-      rated: { $in: ["PG", "G"] },
-      languages: { $all: ["English", "Spanish"] },
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        "imdb.rating": { $gte: 7 },
+        genres: { $nin: ["Crime", "Horror"] },
+        rated: { $in: ["PG", "G"] },
+        languages: { $all: ["English", "Spanish"] },
+      },
     },
-  },
-  {
-    $project: {
-      _id: 0,
-      titulo: "$title",
-      avaliacao: "$rated",
-      notaIMDB: "$imbd.rating",
-      votosIMDB: "$imbd.votes",
-      ano: "$year",
+    {
+      $project: {
+        _id: 0,
+        titulo: "$title",
+        avaliado: "$rated",
+        notaIMDB: "$imdb.rating",
+        votosIMDB: "$imdb.votes",
+        ano: "$year",
+      },
     },
-  },
-]);
+  ],
+);
