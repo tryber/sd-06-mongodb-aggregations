f172ca705c3eb29f33766b5f78a2c2ae53108006
Renan Cunha <renan2610@gmail.com>
2021-01-27 16:27:00 -0300

    Fixing challenge 2 and adding challenge 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -7,4 +7,5 @@ db.movies.aggregate([
   } },
   { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year",
   } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } },
 ]);
