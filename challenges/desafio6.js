3b846c25bf148bf4b98373c53a82151f9a0037b4
pa-volpin <paulo.volpin@gmail.com>
2021-01-27 20:09:09 -0300

    Correction on Task 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -10,7 +10,7 @@ db.movies.aggregate([
   { $project: {
     _id: 0,
     maior_rating: { $round: ["$maior_rating", 1] },
-    menor_rating: { $round: ["$maior_rating", 1] },
+    menor_rating: { $round: ["$menor_rating", 1] },
     media_rating: { $round: ["$media_rating", 1] },
     desvio_padrao: { $round: ["$desvio_padrao", 1] },
   } },
