77d75ac023e425f649d16cf4ea7851ea69d2b07a
Ana Higo <anahigo.arq@gmail.com>
2021-01-27 15:28:03 -0300

    Desafio 1, 2 e 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,17 +1,21 @@
-/*
-Desafio 3
-Agora que você tem os campos essenciais, retorne esses filmes
-ordenados por ano e nota IMDB de forma decrescente e o título
-por ordem alfabética (nesta ordem de desempate).
-
-O resultado da sua query deve ter o seguinte formato:
-
-{
-  "titulo" : "McFarland, USA",
-  "avaliado" : "PG",
-  "notaIMDB" : 7.5,
-  "votosIMDB" : 14091,
-  "ano" : 2015
-}
-// Demais documentos
-*/
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] },
+  } },
+  { $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+  } },
+  { $sort: {
+    ano: -1,
+    notaIMDB: -1,
+    titulo: 1,
+  } },
+]);
