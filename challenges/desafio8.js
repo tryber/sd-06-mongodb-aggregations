74491122bc254d68c32e19deff48a122a4e24701
Ana Higo <anahigo.arq@gmail.com>
2021-01-27 19:30:09 -0300

    Desafio 8 e 9

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,13 +1,17 @@
-/*
-Trocando de contexto, vamos utilizar nosso outro dataset que
-contém dados de empresas aéreas, suas rotas, seus voos e parcerias.
-
-Liste todas as parcerias da coleção air_alliances, que voam rotas
-com um Boing 747 ou um Airbus A380 (que estão abreviados para 747 e 380
-  no campo airplane na coleção air_routes, respectivamente), e descubra
-  qual delas tem o maior número de rotas com esses aviões.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "_id" : <nome_da_alianca>, "totalRotas" : <total_de_rotas> }
-*/
+db.air_alliances.aggregate([
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "rotas",
+  } },
+  { $unwind: "$rotas" },
+  { $match: {
+    "rotas.airplane": { $in: ["747", "380"] },
+  } },
+  { $group: {
+    _id: "$name", totalRotas: { $sum: 1 },
+  } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,13 +1,13 @@
-/*
-Desafio 9
-A partir da coleção trips, determine o menor e o maior ano de nascimento.
-Guarde essa informação, você precisará dela mais tarde.
-
-Não considere documentos com valores vazios ("") ou em que o campo não existe!
-
-Para este desafio utilize o operador $toInt para converter de string para valor inteiro.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "maiorAnoNascimento" : <ano>, "menorAnoNascimento" : <ano> }
-*/
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: 1, $ne: "" } } },
+  { $group: {
+    _id: null,
+    maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+    menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+  } },
+  { $project: {
+    _id: 0,
+    maiorAnoNascimento: 1,
+    menorAnoNascimento: 1,
+  } },
+]);
