bb56a10aa59251b19aee5c35b92a61642637b06e
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-27 19:09:06 -0300

    desafio 3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -6,12 +6,30 @@ db.movies.aggregate([
       { rated: { $in: ["PG", "G"] } },
       { languages: { $all: ["English", "Spanish"] } },
     ] } },
-  { $addFields: {
+  { $project: {
+    _id: 0,
     titulo: "$title",
     avaliado: "$rated",
     notaIMDB: "$imdb.rating",
     votosIMDB: "$imdb.votes",
     ano: "$year",
   } },
-  { $project: { _id: 0, titulo: 1, avaliado: 1, notaIMDB: 1, votosIMDB: 1, ano: 1 } },
 ]);
+
+// db.movies.aggregate([
+//   { $match: {
+//     $and: [
+//       { "imdb.rating": { $gte: 7 } },
+//       { genres: { $not: { $in: ["Crime", "Horror"] } } },
+//       { rated: { $in: ["PG", "G"] } },
+//       { languages: { $all: ["English", "Spanish"] } },
+//     ] } },
+//   { $addFields: {
+//     titulo: "$title",
+//     avaliado: "$rated",
+//     notaIMDB: "$imdb.rating",
+//     votosIMDB: "$imdb.votes",
+//     ano: "$year",
+//   } },
+//   { $project: { _id: 0, titulo: 1, avaliado: 1, notaIMDB: 1, votosIMDB: 1, ano: 1 } },
+// ]);
