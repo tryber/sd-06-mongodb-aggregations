5c054484338bcb190cfc5fbc7da94641590d4eb5
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-27 19:44:18 -0300

    Solução desafio 05 e 06

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,24 +1,20 @@
 db.movies.aggregate([
-  {
-    $match: {
-      countries: { $all: ["USA"] },
-      "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
-    },
-  },
-  {
-    $project: {
-      _id: 0,
-      "tomatoes.viewer.rating": 1,
-      title: 1,
-      num_favs: { $size: { $setIntersection: ["$cast",
-        ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]] } },
-    },
-  },
-  {
-    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
-  },
+  { $match: {
+    countries: { $all: ["USA"] },
+    "tomatoes.viewer.rating": { $gte: 3 },
+    cast: { $in:
+      ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
+  } },
+  { $addFields: {
+    num_favs: { $size:
+      { $setIntersection:
+        ["$cast", ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]],
+      } },
+  } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $project: {
+    _id: 0,
+    title: 1 } },
   { $skip: 24 },
   { $limit: 1 },
-  { $project: { title: 1 } },
 ]);
