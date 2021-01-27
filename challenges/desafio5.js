6c21e2b492d79564b29b4bb9de2f09ce3594b454
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-27 19:11:01 -0300

    :bug: Desafio 05 (Correçao 01)

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -5,7 +5,7 @@ db.movies.aggregate([
   { $addFields: { commonFavs: { $setIntersection: [favArray, "$cast"] } } },
   { $addFields: { num_favs: { $size: "$commonFavs" } } },
   { $project: { _id: false, title: true } },
-  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1} },
-  { $skip: 24},
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $skip: 24 },
   { $limit: 1 },
 ]);
