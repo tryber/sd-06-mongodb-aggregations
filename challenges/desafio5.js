316171aa9da3fdf1a93d552d324fc9c9bccbee81
Isa Koga <isadorakoga710@gmail.com>
2021-01-27 16:48:39 -0300

    Linter fix

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,29 +1,29 @@
 db.movies.aggregate([
   { $match: {
-      "tomatoes.viewer.rating": { $gte: 3 },
-      countries: "USA",
-      cast: { $exists: 1 },
+    "tomatoes.viewer.rating": { $gte: 3 },
+    countries: "USA",
+    cast: { $exists: 1 },
   } },
   { $addFields: {
-      num_favs: {
-        $size: {
-          $setIntersection: [
-            "$cast",
-            [
-              "Sandra Bullock",
-              "Tom Hanks",
-              "Julia Roberts",
-              "Kevin Spacey",
-              "George Clooney"
-            ],
+    num_favs: {
+      $size: {
+        $setIntersection: [
+          "$cast",
+          [
+            "Sandra Bullock",
+            "Tom Hanks",
+            "Julia Roberts",
+            "Kevin Spacey",
+            "George Clooney",
           ],
-        },
+        ],
       },
+    },
   } },
   { $sort: {
-      num_favs: -1,
-      "tomatoes.viewer.rating": -1,
-      title: -1,
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
   } },
   { $project: {
     _id: 0,
