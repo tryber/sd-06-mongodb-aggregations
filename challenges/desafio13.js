a90c5b463a1915690534bce4ffddba0727760ab0
LeandroParisi <lepcbelisario@gmail.com>
2021-01-27 18:55:04 -0300

    feat: desafio 13.2

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -2,21 +2,27 @@ db.trips.aggregate([
   {
     $match: {
       startTime: {
-        $gte: ISODate("2016-03-10"),
-        $lt: ISODate("2016-03-11"),
+        $lt: ISODate("2016-03-11T00:00:00Z"),
+        $gte: ISODate("2016-03-10T00:00:00Z"),
       },
     },
   },
   {
     $group: {
-      _id: null,
-      avgDuration: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60] } },
+      _id: 0,
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+        },
+      },
     },
   },
   {
     $project: {
+      duracaoMediaEmMinutos: {
+        $ceil: "$duracaoMediaEmMinutos",
+      },
       _id: 0,
-      duracaoMediaEmMinutos: { $ceil: "$avgDuration" },
     },
   },
 ]);
