f4936c4917bc0efb33e983006b5ff45144609783
CristianoNunes <cristianonunes99@gmail.com>
2021-01-27 18:43:08 -0300

    Desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,34 @@
+db.movies.aggregate([
+  {
+    $match: {
+      countries: { $all: ["USA"] },
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] }
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      "tomatoes.viewer.rating": 1,
+      title: 1,
+      num_favs: { $size: { $setIntersection: ["$cast",
+      [
+        "Sandra Bullock",
+        "Tom Hanks",
+        "Julia Roberts",
+        "Kevin Spacey",
+        "George Clooney",
+      ]] } },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { title: 1 } },
+]);
