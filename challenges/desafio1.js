629e4aece643c8c37b42f2fb45f2f023f4840b63
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-27 18:37:52 -0300

    estrutura

---
+++ challenges/desafio1.js
@@ -0,0 +1,16 @@
+// db.produtos.updateMany(
+//   {},
+//   { $set: { criadoPor: "Ronald McDonald" } },
+// );
+
+// db.produtos.find(
+//   {},
+//   { nome: 1, criadoPor: 1, _id: 0 },
+// );
+
+// referencias
+// https://docs.mongodb.com/manual/tutorial/model-monetary-data/
+// https://stackoverflow.com/questions/8218484/mongodb-inserts-float-when-trying-to-insert-integer
+// https://docs.mongodb.com/manual/reference/method/db.collection.updateMany/
+// https://docs.mongodb.com/manual/reference/operator/update/currentDate/
+// https://docs.mongodb.com/manual/reference/operator/query/regex/#regex-case-insensitive
