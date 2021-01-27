2521e5f8b20b1761d5aee0121ff348b07b218c90
pa-volpin <paulo.volpin@gmail.com>
2021-01-27 15:35:55 -0300

    Task 3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,6 +3,5 @@ db.movies.aggregate([
   { $match: { $nor: [{ genres: "Crime" }, { genres: "Horror" }] } },
   { $match: { $or: [{ rated: "PG" }, { rated: "G" }] } },
   { $match: { $and: [{ languages: "English" }, { languages: "Spanish" }] } },
-  { $project: { _id: 0, title: 1, rated: 1, "imdb.rating": 1, "imdb.votes": 1, year: 1 } },
-  { $project: { title: "titulo", rated: "avaliado", "imdb.rating": "notaIMDB", "imdb.votes": "votosIMDB", year: "ano" } },
+  { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
 ]);
