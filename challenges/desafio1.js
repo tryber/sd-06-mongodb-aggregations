25015610f0f927e53832c14af544943fa5d947ef
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-27 17:57:23 -0300

    Desafios 1 a 3.

---
+++ challenges/desafio1.js
@@ -0,0 +1,18 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      "genres": {
+        $not: {
+          $in: ["Crime", "Horror"]
+        }
+      },
+      "rated": {
+        $in: ["G", "PG"]
+      },
+      "languages": {
+        $all: ["English", "Spanish"]
+      }
+    }
+  }
+]);
