811d15378bd8c64a115a1afaf4acbd184bc9c05c
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-01-27 17:27:01 -0300

    Desafio 1 feito

---
+++ challenges/desafio1.js
@@ -0,0 +1,13 @@
+db.movies.aggregate(
+  [{ $match : {
+    $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { $nor: [
+        { genres: { $elemMatch: { $eq: "Crime" } }},
+        { genres: { $elemMatch: { $eq: "Horror" } }}
+      ] },
+      { $or: [{ rated: "PG"}, { rated: "G"}]},
+      { $and: [{ languages: "English"}, { languages: "Spanish"}]}
+    ] 
+  } }]
+);
