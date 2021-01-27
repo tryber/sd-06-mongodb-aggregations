82fa44b0ad0bee4d798ff342b954138adc637ea5
LeandroParisi <lepcbelisario@gmail.com>
2021-01-27 15:06:41 -0300

    feat: desafio 4.2

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -3,12 +3,29 @@ db.movies.aggregate([
     $project: {
       title_split: { $split: ["$title", " "] },
       _id: 0,
+      title: 1,
     },
   },
   {
     $project: {
       title_split: 1,
+      title: 1,
       title_size: { $size: "$title_split" },
+    },
+  },
+  {
+    $match: {
+      title_size: 1,
+    },
+  },
+  {
+    $sort: {
+      title: 1,
+    },
+  },
+  {
+    $project: {
+      title_split: 1,
     },
   },
 ]);
