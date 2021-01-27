d70105fd6f8889d37e8ed9cc3a546b8719169a39
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-27 15:31:30 -0300

    desafios 1 e 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -9,4 +9,5 @@ db.movies.aggregate([
       ],
     },
   },
+  { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
 ]);
