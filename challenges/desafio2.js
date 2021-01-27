fa8aff53305f78be3f6fb991ed7f2df1f57e6aa9
higoramorim <1810042@aluno.univesp.br>
2021-01-27 18:39:47 -0300

    des 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,21 @@
+db.movies.aggregate([
+  {
+    $match: { $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $nin: ["Crime", "Horror"] } },
+      { rated: { $in: ["PG", "G"] } },
+      { languages: { $all: ["English", "Spanish"] } },
+    ],
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imbd.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+]);
