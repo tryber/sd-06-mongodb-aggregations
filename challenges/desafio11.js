6b963909128d16b513483a5fabf5f9dd0c9b7fd9
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-28 10:46:14 -0300

    :bug: Desafio 11 (Correçao 01)

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,9 +1,9 @@
 db.trips.aggregate([
   { $group: {
-    _id: "$startTime",
+    _id: { $dayOfWeek: "$startTime" },
     total: { $sum: 1 },
   } },
-  { $project: { _id: false, diaDaSemana: { $dayOfWeek: "$_id" }, total: true } },
+  { $project: { _id: false, diaDaSemana: "$_id", total: true } },
   { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
