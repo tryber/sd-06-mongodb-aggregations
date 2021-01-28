267be4053dee5ec1e582fab7d86a6169d91b8056
luizmacmillan <luizmacmillan@gmail.com>
2021-01-28 16:55:57 -0300

    Desafio 5.

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,9 +1,11 @@
 db.movies.aggregate([
   {
-    $project: {
-      _id: 0,
+    $addFields: {
       title_split: { $split: ["$title", " "] },
     },
+  },
+  {
+    $sort: { title: 1 },
   },
   {
     $match: {
@@ -11,6 +13,9 @@ db.movies.aggregate([
     },
   },
   {
-    $sort: { title: 1 },
+    $project: {
+      _id: 0,
+      title_split: 1,
+    },
   },
 ]);
