b3c02ed55883cb511fce4ed1c2ab91afbaf7436a
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2021-01-28 15:45:19 -0300

    Challenge 05 | FIX

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -9,37 +9,30 @@
 // Ordene os resultados por num_favs, tomatoes.viewer.rating
 // e title, todos em ordem decrescente.
 
-const favoriteActors = [
-  "Sandra Bullock",
-  "Tom Hanks",
-  "Julia Roberts",
-  "Kevin Spacey",
-  "George Clooney",
-];
-
 db.movies.aggregate([
   {
     $match: {
-      countries: { $all: ["USA"] },
+      cast: {
+        $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+      },
+      countries: "USA",
       "tomatoes.viewer.rating": { $gte: 3 },
     },
   },
   {
-    $project: {
-      _id: 0,
-      "tomatoes.viewer.rating": 1,
-      title: 1,
-      num_favs: { $size: { $setIntersection: ["$cast", favoriteActors] } },
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: [
+            ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+            "$cast",
+          ],
+        },
+      },
     },
   },
-  {
-    $sort: {
-      num_favs: -1,
-      "tomatoes.viewer.rating": -1,
-      title: -1,
-    },
-  },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $project: { _id: 0, title: 1 } },
   { $skip: 24 },
   { $limit: 1 },
-  { $project: { title: 1 } },
 ]);
