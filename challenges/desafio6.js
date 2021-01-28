ac14b4000e311e4757da4b0dfa70542a23fe6ca4
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-27 22:30:37 -0300

    desafio6 correction

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,38 +1,6 @@
 db.movies.aggregate([
-  {
-    $addFields:
-      {
-        result:
-          {
-            $regexMatch:
-                  { input: "$awards", regex: /won \d+ oscars?/gi },
-
-          },
-      },
-  },
+  { $addFields: { result: { $regexMatch: { input: "$awards", regex: /won \d+ oscars?/gi } } } },
   { $match: { result: true } },
-  {
-    $group:
-      {
-        _id: "$result",
-        maior_rating:
-              { $max: "$imdb.rating" },
-        menor_rating: { $min: "$imdb.rating" },
-        media_rating: { $avg: "$imdb.rating" },
-        desvio_padrao: { $stdDevSamp: "$imdb.rating" },
-
-      },
-  },
-  {
-    $project:
-      {
-        _id: 0,
-        maior_rating: 1,
-        menor_rating: 1,
-        media_rating:
-              { $round: ["$media_rating", 1] },
-        desvio_padrao: { $round: ["$desvio_padrao", 1] },
-
-      },
-  },
+  { $group: { _id: "$result", maior_rating: { $max: "$imdb.rating" }, menor_rating: { $min: "$imdb.rating" }, media_rating: { $avg: "$imdb.rating" }, desvio_padrao: { $stdDevSamp: "$imdb.rating" } } },
+  { $project: { _id: 0, maior_rating: 1, menor_rating: 1, media_rating: { $round: ["$media_rating", 1] }, desvio_padrao: { $round: ["$desvio_padrao", 1] } } },
 ]);
