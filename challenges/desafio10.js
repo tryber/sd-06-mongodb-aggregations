1c7a947024c28caabcde60269364705938471342
phellipe5k <luizinhophe@hotmail.com>
2021-01-28 18:53:14 -0300

    challenge 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,1 @@
+db.trips.aggregate([{ $addFields: { minutes: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } } }, { $group: { _id: "$usertype", media: { $avg: "$minutes" } } }, { $project: { _id: 0, tipo: "$_id", duracaoMedia: { $round: ["$media", 2] } } }, { $sort: { duracaoMedia: 1 } }]);
