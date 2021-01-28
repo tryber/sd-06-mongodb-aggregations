17721c3d3e914e998b0681916502e8eb80ecf6da
phellipe5k <luizinhophe@hotmail.com>
2021-01-28 19:21:36 -0300

    challenge 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,1 @@
+db.trips.aggregate([{ $match: { startTime: { $gte: ISODate("2016-03-10T00:00:00Z"), $lte: ISODate("2016-03-10T23:59:59Z") } } }, { $addFields: { minutes: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60] } } }, { $group: { _id: null, media: { $avg: "$minutes" } } }, { $project: { _id: 0, duracaoMediaEmMinutos: { $ceil: "$media" } } }]);
