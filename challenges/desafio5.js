6f5bab7f153395c1a5e45d3e5a3a07499570c941
Ana Higo <anahigo.arq@gmail.com>
2021-01-28 18:44:25 -0300

    Desafio 4 e 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,28 +1,34 @@
-/*
-Desafio 5
-Temos outra noite de filme aqui na Trybe e, desta vez, nós perguntamos à
-equipe quais são seus atores ou atrizes preferidos. Aqui está o resultado:
-
-Sandra Bullock
-Tom Hanks
-Julia Roberts
-Kevin Spacey
-George Clooney
-Para filmes lançados nos Estados Unidos (campo countries), com
-tomatoes.viewer.rating maior ou igual a 3, crie um novo campo
-chamado num_favs, que represente quantos atores ou atrizes da
-nossa lista de favoritos aparecem no elenco (campo cast) do filme.
-
-Ordene os resultados por num_favs, tomatoes.viewer.rating e title,
-todos em ordem decrescente.
-
-Por fim, utilizando o mesmo pipeline, responda: Qual o título do vigésimo
-quinto filme do resultado dessa agregação?
-
-Dica: coloque a lista de atores e atrizes favoritos em uma variável e
-explore operadores como $size e $setIntersection.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "title" : <nome_do_filme> }
-*/
+db.movies.aggregate([
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+  } },
+  { $addFields: {
+    matchingActors: {
+      $setIntersection: [[
+        "Sandra Bullock",
+        "Tom Hanks",
+        "Julia Roberts",
+        "Kevin Spacey",
+        "George Clooney",
+      ], "$cast"],
+    },
+  } },
+  { $match: {
+    matchingActors: { $ne: null },
+  } },
+  { $addFields: {
+    num_favs: { $size: "$matchingActors" },
+  } },
+  { $sort: {
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $project: {
+    title: 1,
+    _id: 0,
+  } },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
