e8518a749012c2f7f7f9f53aabcd21a2f082b7ee
pa-volpin <paulo.volpin@gmail.com>
2021-01-28 15:47:11 -0300

    Correction on Task 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -6,7 +6,7 @@ db.movies.aggregate([
     numeroFilmes: { $sum: 1 },
     mediaIMDB: { $avg: "$imdb.rating" },
   } },
-  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: "$mediaIMDB" },
+  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] },
   } },
   { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);
