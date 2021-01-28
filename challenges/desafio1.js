0037f4f9ef6c87dac809f0b94e1236e6a05130d4
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-01-28 19:18:10 -0300

    ate 5

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,10 @@
-db.movies.aggregate([]);
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Horror", "Crime"] },
+      rated: { $in: ["PG", "G", "UNRATED"] },
+      $and: [{ languages: { $eq: "English" } }, { languages: { $eq: "Spanish" } }]
+    } 
+  }
+]);
