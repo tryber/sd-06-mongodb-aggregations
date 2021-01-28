8f91bbdbb22d3362826cc41a33972c240da99a77
Renan Cunha <renan2610@gmail.com>
2021-01-28 15:26:06 -0300

    Adding challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -8,6 +8,6 @@ db.trips.aggregate([
     _id: "$usertype",
     duracaoMedia: { $avg: "$duracao" },
   } },
-  { $project: { duracaoMedia: { $round: ["$duracaoMedia", 2] } } },
+  { $project: { _id: 0, tipo: "$_id", duracaoMedia: { $round: ["$duracaoMedia", 2] } } },
   { $sort: { duracaoMedia: 1 } },
 ]);
