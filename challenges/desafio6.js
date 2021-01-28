e0f55314b5232b6ffb5475bebf07ccf702b4a274
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-28 16:10:24 -0300

    Sixth challenge

---
+++ challenges/desafio6.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([
+  {
+    $match: {
+      awards: {
+        $regex: /^Won \d+ Oscar/,
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: {
+        $max: "$imdb.rating",
+      },
+      menor_rating: {
+        $min: "$imdb.rating",
+      },
+      media_rating: {
+        $avg: "$imdb.rating",
+      },
+      desvio_padrao: {
+        $stdDevSamp: "$imdb.rating",
+      },
+    },
+  },
+]);
