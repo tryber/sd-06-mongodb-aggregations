b63abd1265b472b829d071b203e75d90180aec44
higoramorim <1810042@aluno.univesp.br>
2021-01-28 18:01:20 -0300

    18:01

---
+++ challenges/desafio13.js
@@ -0,0 +1,9 @@
+db.trips.aggregate([
+  { $match:
+    { startTime:
+      { $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lte: ISODate("2016-03-11T00:00:00Z") } } },
+  { $addFields: { duracao:
+          { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] } } },
+  { $group: { _id: 0, media: { $avg: "$duracao" } } },
+  { $project: { _id: 0, duracaoMediaEmMinutos: { $ceil: "$media" } } }]);
