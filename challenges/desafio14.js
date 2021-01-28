946a05a8f8866065f1fb8405d71ac4745c3850b6
phellipe5k <luizinhophe@hotmail.com>
2021-01-28 19:27:34 -0300

    challenge 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,1 @@
+db.trips.aggregate([{ $addFields: { minutes: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60] } } }, { $group: { _id: "$bikeid", media: { $avg: "$minutes" } } }, { $project: { _id: 0, bikeId: "$_id", duracaoMedia: { $ceil: "$media" } } }, { $sort: { duracaoMedia: -1 } }, { $limit: 5 }]);
