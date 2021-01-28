e095fddab0bda75be742d65604717ccbc583dd4c
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-28 20:26:20 -0300

    Requirement 12 - push 01

---
+++ challenges/desafio12.js
@@ -0,0 +1,27 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemanaMaiorNumViagens: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      diaDaSemanaMaiorNumViagens: 5,
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
