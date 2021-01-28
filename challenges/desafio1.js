c29c1c688f0958ff38a3c807912851666b41c5ed
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-28 04:08:25 -0300

    desafio 1 ao 4

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,8 +1,8 @@
 db.movies.aggregate([
-  { $match: { 
+  { $match: {
     "imdb.rating": { $gte: 7 },
     genres: { $nin: ["Crime", "Horror"] },
     rated: { $in: ["PG", "G"] },
-    languages: { $all: ["English", "Spanish"] }
-  } }
-])
+    languages: { $all: ["English", "Spanish"] },
+  } },
+]);
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,9 +1,9 @@
 db.movies.aggregate([
-  { $match: { 
+  { $match: {
     "imdb.rating": { $gte: 7 },
     genres: { $nin: ["Crime", "Horror"] },
     rated: { $in: ["PG", "G"] },
-    languages: { $all: ["English", "Spanish"] }
+    languages: { $all: ["English", "Spanish"] },
   } },
   { $project: {
     _id: 0,
@@ -12,5 +12,5 @@ db.movies.aggregate([
     notaIMDB: "$imdb.rating",
     votosIMDB: "$imdb.votes",
     ano: "$year",
-  } }
-])
+  } },
+]);
