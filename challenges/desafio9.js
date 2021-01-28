010c25db7fe92368fa535374073a5de46f0535a9
Ana Flávia <anaflvia@gmail.com>
2021-01-28 15:25:44 -0300

    desafio 9 corrigido e desafio 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -17,4 +17,11 @@ db.trips.aggregate([
       menorAnoNascimento: { $min: "$birthYear" },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
 ]);
