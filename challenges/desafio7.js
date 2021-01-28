21e01a3acc85685ff3d4b5a559ea3dc30fe1a199
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-28 14:56:45 -0300

    Solução desafio 07

---
+++ challenges/desafio7.js
@@ -0,0 +1,25 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $all: ["English"] },
+    },
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: { numeroFilmes: -1, mediaIMDB: -1 },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+]);
