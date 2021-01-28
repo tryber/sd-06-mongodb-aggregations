c2e74f024be9919a2e07d44d7683a4372b2189c4
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-28 19:00:59 -0300

    Tenth challenge

---
+++ challenges/desafio10.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      tripTime: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      avg_trip: { $avg: "$tripTime" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: [{ $divide: ["$avg_trip", 1000 * 60 * 60] }, 2] },
+    },
+  },
+]);
