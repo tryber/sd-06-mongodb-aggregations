a6b765d06539070cf030e877bdc08df12ed4bf04
Renan Cunha <renan2610@gmail.com>
2021-01-28 09:40:55 -0300

    Fixing challenge 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,27 +1,4 @@
 db.air_alliances.aggregate([
-  { $unwind: "$airlines" },
-  { $lookup: {
-    from: "air_routes",
-    let: { company: "$airlines" },
-    pipeline: [
-      {
-        $match: {
-          airplane: { $in: ["747", "380"] },
-          $expr: { $eq: ["$$company", "$airline.name"] },
-        },
-      },
-    ],
-    as: "routes",
-  } },
-  { $group: {
-    _id: "$name",
-    totalRotas: { $sum: { $size: "$routes" } },
-  } },
-  { $sort: { totalRotas: -1 } },
-  { $limit: 1 },
-]);
-
-db.air_alliances.aggregate([
   { $lookup: {
     from: "air_routes",
     localField: "airlines",
