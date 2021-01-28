9867b4d3e1447b39090af32f65d3d11a2db32d6b
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-28 18:42:23 -0300

    :bug: Desafio 14 (Correçao 01)

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,7 +1,14 @@
 db.trips.aggregate([
   { $group: {
     _id: "$bikeid",
-    duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] } } },
+    duracaoMedia: { $avg: 
+      { $divide: [
+        { $subtract: ["$stopTime", "$startTime"] },
+         60000
+        ] 
+      },
+    },
+  },
   },
   { $project: { _id: false, bikeid: "$_id", duracaoMedia: { $ceil: "$duracaoMedia" } } },
   { $sort: { duracaoMedia: -1 } },
