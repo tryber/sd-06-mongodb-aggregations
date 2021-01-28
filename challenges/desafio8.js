5ea38eda82b2876cd4ef21b7f17227d7b82dda55
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-28 15:28:27 -0300

    Solução desafio 09

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,18 +1,20 @@
 db.air_alliances.aggregate([
-  { $lookup: {
-    from: "air_routes",
-    localField: "airlines",
-    foreignField: "airline.name",
-    as: "routes",
-  } },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "routes",
+    },
+  },
   { $unwind: "$routes" },
-  { $match: {
-    "routes.airplane": { $in: ["747", "380"] },
-  } },
-  { $group: {
-    _id: "$name",
-    totalRotas: { $sum: 1 },
-  } },
+  { $match: { "routes.airplane": { $in: ["747", "380"] } } },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
   { $sort: { totalRotas: -1 } },
   { $limit: 1 },
 ]);
