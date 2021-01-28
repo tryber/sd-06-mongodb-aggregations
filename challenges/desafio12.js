791b900e207f9eec02413937b4c0b1edd73ff856
Isa Koga <isadorakoga710@gmail.com>
2021-01-28 20:33:13 -0300

    :rotating_light: Fix challenge 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -4,13 +4,13 @@ db.trips.aggregate([
   } },
   { $group: {
     _id: "$startStationName",
-    total: { $sum: 1 },
+    count: { $sum: 1 },
   } },
-  { $sort: { total: -1 } },
-  { $limit: 1 },
   { $project: {
     _id: 0,
     nomeEstacao: "$_id",
-    total: "$total",
+    total: "$count",
   } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
 ]);
