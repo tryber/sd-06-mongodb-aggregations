658fae7a69d26780804275163b4d18a29e05a712
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-28 18:46:34 -0300

    Requirement 09 - push 01

---
+++ challenges/desafio9.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $ne: "" },
+    },
+  },
+  {
+    $addFields: {
+      intBirthYear: { $toInt: "$birthYear" },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: "$intBirthYear" },
+      menorAnoNascimento: { $min: "$intBirthYear" },
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
