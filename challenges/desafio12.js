013269e4101a32a9654249a4a1bb1d08a6b0f392
Ana Higo <anahigo.arq@gmail.com>
2021-01-28 13:53:06 -0300

    Desafio 12, 13 e 14

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,13 +1,18 @@
-/*
-Desafio 12
-Agora que você já sabe o dia com mais viagens, determine qual estação
-tem o maior número de viagens nesse dia da semana. Mas, para isso,
-adicione o que for necessário ao pipeline anterior. Exiba apenas o nome
-da estação e o total de viagens.
-
-Dica: Utilize o operador $dayOfWeek para extrair o dia da semana como um número de uma data.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "nomeEstacao" : <nome_da_estacao>, "total" : <total_de_viagens> }
-*/
+db.trips.aggregate([
+  { $match: {
+    $expr: {
+      $eq: [{ $dayOfWeek: "$startTime" }, 5],
+    },
+  } },
+  { $group: {
+    _id: "$startStationName",
+    total: { $sum: 1 },
+  } },
+  { $project: {
+    _id: 0,
+    nomeEstacao: "$_id",
+    total: "$total",
+  } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,8 +1,23 @@
-/*
-Determine a duração média das viagens iniciadas no dia 10/03/2016, em minutos.
-Arredonde o resultado para cima.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "duracaoMediaEmMinutos" : <duracao_media_em_minutos> }
-*/
+db.trips.aggregate([
+  { $match: {
+    startTime: {
+      $gte: ISODate("2016-03-10T00:00:00Z"),
+      $lt: ISODate("2016-03-11T00:00:00Z"),
+    },
+  } },
+  { $group: {
+    _id: null,
+    duracaoMediaEmMinutos: {
+      $avg: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60000, /* milisseg * seg */
+        ],
+      },
+    },
+  } },
+  { $project: {
+    _id: 0,
+    duracaoMediaEmMinutos: { $ceil: ["$duracaoMediaEmMinutos"] },
+  } },
+]);
