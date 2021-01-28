b9d3fdbf5f7cca98f46dd96e577b59f329e1f801
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-28 16:33:30 -0300

    Sexth challenge corrected, with rounded answers

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -15,11 +15,24 @@ db.movies.aggregate([
       menor_rating: {
         $min: "$imdb.rating",
       },
-      media_rating: {
+      avg_rating: {
         $avg: "$imdb.rating",
       },
-      desvio_padrao: {
+      std_dev_rating: {
         $stdDevSamp: "$imdb.rating",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: {
+        $round: ["$avg_rating", 1],
+      },
+      desvio_padrao: {
+        $round: ["$std_dev_rating", 1],
       },
     },
   },
