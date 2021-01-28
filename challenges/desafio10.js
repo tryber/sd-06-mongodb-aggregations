d9b21e416035500ab362dbbbadc3567c183f0ac6
bynho25 <pablo.trybe@gmail.com>
2021-01-28 19:26:45 -0300

    Fix

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,21 +1,20 @@
 db.trips.aggregate([
-  { $group: {
-    _id: "$userType",
-    duracaoMedia: {
-      $avg: {
-        $divide: [
-          { $subtract: ["$stopTime", "$startTime"] },
-          3600000,
-        ],
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 60 * 1000],
+        },
       },
     },
-  } },
-  { $project: {
-    tipo: "$_id",
-    duracaoMedia: { $round: ["$duracaoMedia", 2] },
-    _id: 0,
-  } },
-  { $sort: {
-    duracaoMedia: 1,
-  } },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+  { $sort: { duracaoMedia: 1 } },
 ]);
