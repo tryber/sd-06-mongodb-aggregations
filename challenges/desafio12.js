27cbff6b9e4c317bd6a1fe625d07500107d7b531
pa-volpin <paulo.volpin@gmail.com>
2021-01-28 20:40:59 -0300

    Changes on Task 11

---
+++ challenges/desafio12.js
@@ -0,0 +1,18 @@
+db.trips.aggregate([
+  { $lookup: {
+    from: "trips",
+    let: { tripDate: "$startTime" },
+    pipeline: [
+      { $project: { dia: { $dayOfWeek: "$startTime" } } },
+      { $group: { _id: "$dia", totalX: { $sum: 1 } } },
+      { $sort: { totalX: -1 } },
+      { $limit: 1 },
+      { $match: { _id: { $eq: { $dayOfWeek: "$$tripDate" } } } },
+    ],
+    as: "tripsInMaxDay",
+  } },
+  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
