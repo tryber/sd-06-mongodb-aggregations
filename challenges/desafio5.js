3d524d06ab93130c9739dcc76162f629aabd6bf0
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-28 18:57:02 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,30 @@
+
+db.movies.aggregate([
+  {
+    $match:
+    {
+      countries: {
+        $eq: "USA"
+      },
+      "tomatoes.viewer.rating": {
+        $gte: 3
+      },
+        cast: { $exists:1}
+  }
+  },
+  {
+    $addFields: {
+      num_favs: { $size:{ $setIntersection: [ "$cast", ["Sandra Bullock", "Julia Roberts",
+      "Tom Hanks", "Kevin Spacey", "George Clooney"]] },
+    },
+  },
+  {
+    $skip:24
+    },
+  {
+    $limit: 1
+    },
+  {
+    $sort: { "tomatoes.viewer.rating": -1, title: -1 },
+  },
+]);
