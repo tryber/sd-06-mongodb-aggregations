733b80c158ccc7e7055e82273bbc7d5a8c4cb6c2
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-01-28 11:28:54 -0300

    Subindo desafio 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,15 +1,23 @@
 db.trips.aggregate([
+  { $addFields: {
+    diaDaSemana: { $dayOfWeek: "$startTime" },
+  },
+  },
+  { $match: {
+    diaDaSemana: 5
+  },
+  },
   {
     $group: {
-      _id: { $dayOfWeek: "$startTime" },
-      diaDaSemana: { $sum: 1 },
+      _id: { nomeEstacao: "$startStationName" },
+      qteDiaDaSemana: { $sum: 1 },
     },
   },
   {
     $project: {
       _id: 0,
-      diaDaSemana: "$_id",
-      total: "$diaDaSemana",
+      nomeEstacao: "$_id",
+      total: "$qteDiaDaSemana",
     },
   },
   {
