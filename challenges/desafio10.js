ff0dc141543977c79cda73d4741fbd73722d19b1
Ana Higo <anahigo.arq@gmail.com>
2021-01-28 00:32:53 -0300

    Desafio 10 e 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,11 +1,19 @@
-/*
-Desafio 10
-Encontre a média de viagens por tipo de usuário.
-Exiba o valor em horas com apenas duas casas decimais e a média de
-viagens ordenada de forma crescente. Para arredondar a média use o $round.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "tipo" : <tipo>, "duracaoMedia" : <duracaoMedia> }
-// ...
-*/
+db.trips.aggregate([
+  { $group: {
+    _id: "$usertype",
+    duracaoMedia: {
+      $avg: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          3600000, /* milisseg (1000) * seg(60) * min(60) */
+        ],
+      },
+    },
+  } },
+  { $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaoMedia: { $round: ["$duracaoMedia", 2] },
+  } },
+  { $sort: { duracaoMedia: 1 } },
+]);
