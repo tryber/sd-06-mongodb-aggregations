016c0268da70934ac8fba765fe24f20786425b85
labelle <beelsdelonge@gmail.com>
2021-01-28 18:00:10 -0300

    Desafio10

---
+++ challenges/desafio10.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diferençaTempo: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      media: { $avg: "$diferençaTempo" },
+    },
+  },
+  {
+    $addFields: {
+      conversaoTempo: { $divide: ["$media", 3600000] },
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      duracaoMedia: { $round: ["$conversaoTempo", 2] },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
+]);
