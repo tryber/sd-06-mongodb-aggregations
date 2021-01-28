d530d713b329dadd9ba9e457599fe9f8984a9184
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-28 18:11:56 -0300

    desafio1

---
+++ challenges/desafio1.js
@@ -0,0 +1,26 @@
+/*
+Ajude a Trybe a escolher um filme para a próxima noite! Baseado em uma pesquisa,
+decidimos que os filmes em potencial devem atender alguns critérios, vejamos:
+
+Retorne todos os filmes que satisfaça, através de uma pipeline, as condições abaixo
+1)imdb.rating deve ser ao menos 7;
+2)genres não deve conter Crime ou Horror;
+3)rated deve ser igual a PG ou G;
+4)languages contém English e Spanish.
+
+-Utilize a coleção movies.
+-Sua query deve retornar 41 documentos.
+
+*/
+
+db.movies.aggregate([
+  {
+    $match:
+    {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+]);
