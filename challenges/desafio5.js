e857045f056ed2ebd57258c9a56709dc3b62ab52
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-28 19:49:29 -0300

    desafio 5 - elemenMatch

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,7 +3,9 @@ db.movies.aggregate([
     $match:
     {
       countries: {
-        $eq: "USA",
+        $elemMatch: {
+          $eq: "USA",
+        },
       },
       "tomatoes.viewer.rating": {
         $gte: 3,
@@ -19,13 +21,20 @@ db.movies.aggregate([
     },
   },
   {
+    $sort: {
+      "tomatoes.viewer.rating": -1, title: -1,
+    },
+  },
+  {
     $skip: 24,
   },
   {
     $limit: 1,
   },
   {
-    $sort: { "tomatoes.viewer.rating": -1, title: -1 },
+    $sort: {
+      "tomatoes.viewer.rating": -1, title: -1,
+    },
   },
   {
     $project: { _id: 0, title: 1 },
