328f1fe9571b41a63887ee956694db9dd78e2240
Ana Flávia <anaflvia@gmail.com>
2021-01-28 16:28:32 -0300

    desafio 11 corrigido e desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -3,19 +3,26 @@ db.trips.aggregate([
     $project: {
       _id: 0,
       diaDaSemana: { $dayOfWeek: "$startTime" },
+      nomeEstacao: "$startStationName",
+    },
+  },
+  {
+    $match: {
+      $expr: {
+        $eq: ["$diaDaSemana", 5],
+      },
     },
   },
   {
     $group: {
-      _id: "$diaDaSemana",
-      diaDaSemana: {$first: "$diaDaSemana"},
+      _id: "$nomeEstacao",
       total: { $sum: 1 },
     },
   },
   {
     $project: {
-      diaDaSemana: 1,
       _id: 0,
+      nomeEstacao: "$_id",
       total: 1,
     },
   },
