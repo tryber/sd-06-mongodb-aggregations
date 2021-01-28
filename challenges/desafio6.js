533aa45752fbc3d7b36a25d3bcfe3932b3af08c4
luizmacmillan <luizmacmillan@gmail.com>
2021-01-28 17:40:49 -0300

    Correçao Desafio 6.

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -16,10 +16,10 @@ db.movies.aggregate([
   {
     $project: {
       _id: 0,
-      maior_rating: { $round: ["$maior_rating", 2] },
-      menor_rating: { $round: ["$menor_rating", 2] },
-      media_rating: { $round: ["$media_rating", 2] },
-      desvio_padrao: { $round: ["$desvio_padrao", 2] },
+      maior_rating: { $round: ["$maior_rating", 1] },
+      menor_rating: { $round: ["$menor_rating", 1] },
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
 ]);
