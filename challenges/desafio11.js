bc8de8da1b35d1cb384965be555b01480f4fc95c
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-28 19:16:22 -0300

    desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      dayOfWeek: {
+        $dayOfWeek: "$startTime", 
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$dayOfWeek",
+      total: {
+        $sum: 1,
+      },
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
