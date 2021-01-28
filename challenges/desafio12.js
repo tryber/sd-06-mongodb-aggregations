a05e36a200593746bc1469170162339686fc2721
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-28 12:44:36 -0300

    :sparkles: Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,10 @@
+db.trips.aggregate([
+  { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, 5] } } },
+  { $group: {
+    _id: "$startStationName",
+    total: { $sum: 1 },
+  } },
+  { $project: { _id: false, nomeEstacao: "$_id", total: "$total" } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
