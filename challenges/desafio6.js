cb799125353c6a76361ae08c645cc3435f9476f8
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-28 15:47:43 -0300

    desafio 6 + corrigindo desafio 4

---
+++ challenges/desafio6.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([
+  { $match: {
+    $and: [
+      { awards: { $exists: true } },
+      { awards: { $regex: /Oscar/ } },
+    ],
+  } },
+  { $project: {
+    _id: 0,
+    rating: "$imdb.rating",
+  } },
+  { $group: {
+    _id: null,
+    maior_rating: { $max: "$rating" },
+    menor_rating: { $min: "$rating" },
+    media_rating: { $avg: "$rating" },
+    desvio_padrao: { $stdDevSamp: "$rating" },
+  } },
+  { $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] },
+  } },
+]);
