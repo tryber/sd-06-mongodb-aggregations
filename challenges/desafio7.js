92aabba0b0994a4bedf12fcccfa18a4ab0902981
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-28 10:53:35 -0300

    desafios 7 - ajusting

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -18,5 +18,5 @@ db.movies.aggregate([
   {
     $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: [{ $divide: ["$somaIDMB", "$numeroFilmes"] }, 1] } },
   },
-  { $sort: { numeroFilmes: -1 } },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);
