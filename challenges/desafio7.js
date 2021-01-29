d2a17d04de25daa2e1362538f4ee2b3454440436
vapatty6848 <vapatty@gmail.com>
2021-01-29 10:10:49 -0300

    desafio 7- erros de lint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,20 +1,20 @@
 db.movies.aggregate([
   {
     $match: {
-      languages: { $in: ['English'] },
+      languages: { $in: ["English"] },
       cast: { $exists: true },
     },
   },
-  { $unwind: '$cast' },
+  { $unwind: "$cast" },
   {
     $group: {
-      _id: '$cast',
+      _id: "$cast",
       numeroFilmes: { $sum: 1 },
-      mediaIMDB: { $avg: '$imdb.rating' },
+      mediaIMDB: { $avg: "$imdb.rating" },
     },
   },
   {
-    $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ['$mediaIMDB', 1] } },
+    $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } },
   },
-  { $sort: { numeroFilmes: -1, _id: -1 }}
+  { $sort: { numeroFilmes: -1, _id: -1 } };
 ]).pretty();
