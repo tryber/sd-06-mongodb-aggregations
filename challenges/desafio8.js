e6c6da43fdecce5dcbaac89e30e1a8efff4fea6d
higoramorim <1810042@aluno.univesp.br>
2021-01-28 21:09:03 -0300

    des 6,7,8

---
+++ challenges/desafio8.js
@@ -0,0 +1,13 @@
+db.air_alliances.aggregate([
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "interAir",
+  } },
+  { $unwind: "$interAir" },
+  { $match: { "interAir.airplane": { $in: ["747", "380"] } } },
+  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
