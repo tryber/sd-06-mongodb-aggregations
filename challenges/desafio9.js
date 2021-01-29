23ed72a021a778b95551364b0c2320f250f35e3d
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-28 22:44:09 -0300

    fix desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: true, $nin: [""] } } },
+  { $addFields: { anoNascimento: { $toInt: "$birthYear" } } },
+  { $group: {
+    _id: null,
+    maiorAnoNascimento: { $max: "$anoNascimento" },
+    menorAnoNascimento: { $min: "$anoNascimento" },
+  } },
+  { $project: { _id: 0 } },
+]);
