67794d344a465975c6fa8ee7d23042d36bb5e813
vapatty6848 <vapatty@gmail.com>
2021-01-29 09:55:34 -0300

    desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,40 @@
+/* ### Desafio 7
+
+Vamos nos aprofundar um pouco mais em nossa coleção de filmes. 
+
+#### Conte quantos filmes cada um dos atores e atrizes do elenco (`cast`) já participou
+ e obter 
+uma média do campo `imdb.rating` para cada um desses atores e atrizes.
+
+- Traga o nome do ator ou atriz, número de filmes em que participou e a média do imdb 
+esses 
+filmes arredondada para uma casa decimal usando o operador
+ [`$round`]
+ (https://docs.mongodb.com/manual/reference/operator/aggregation/round/index.html). 
+- Considere somente os membros do elenco de filmes com o idioma inglês (`English`). 
+- Exiba a lista em ordem decrescente de documentos pelo número de filmes e nome do ator ou atriz.
+
+Sua query deve retornar `47055` documentos. Cada documento no resultado deve ter o seguinte formato:
+
+```javascript
+{ "_id" : "John Wayne", "numeroFilmes" : 107, "mediaIMDB" : 6.4 } */
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $in: ['English'] },
+      cast: { $exists: true },
+    },
+  },
+  { $unwind: '$cast' },
+  {
+    $group: {
+      _id: '$cast',
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: '$imdb.rating' },
+    },
+  },
+  {
+    $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ['$mediaIMDB', 1] } },
+  },
+  { $sort: { numeroFilmes: -1, _id: -1 }}
+]).pretty();
