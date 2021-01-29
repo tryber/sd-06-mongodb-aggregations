9b5e51e3e0f09c763ea784518037d22b5c2cab5c
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-28 22:11:23 -0300

    desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  { $match: { languages: "English" } },
+  { $unwind: "$cast" },
+  { $group: {
+    _id: "$cast",
+    avg_rating: { $avg: "$imdb.rating" },
+    numeroFilmes: { $sum: 1 },
+  } },
+  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$avg_rating", 1] } } },
+]);
