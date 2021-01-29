ddf1b6e5e4ca6142e59e119ae79400b7a259c1a1
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-01-29 12:34:42 -0300

    10° tirando os erros de lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,12 +1,12 @@
 db.trips.aggregate([
   {
     $project: {
-      hora: { $hour: new Date("2016-01-01T00:50:00Z") }
+      hora: { $hour: new Date("2016-01-01T00:50:00Z") },
     },
   },
 ]);
-{
-  $project: {
-    _id: 0,
-  },
-},
+// {
+//   $project: {
+//     _id: 0,
+//   },
+// },
