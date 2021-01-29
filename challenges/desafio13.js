90aa5a44e96510f20cc96ff2beb7377a8c44de74
FlavioSugano <flavio.sugano@gmail.com>
2021-01-29 00:02:49 -0300

    Desafio 13: correçao nos criterios utilizados no

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,27 +1,29 @@
 db.trips.aggregate([
   {
     $match: {
-      startTime: { $gte: ISODate("2016-03-10") },
-      stopTime: { $lt: ISODate("2016-03-11") },
+      startTime: {
+        $gte: ISODate("2016-03-10"),
+        $lt: ISODate("2016-03-11"),
+      },
     },
   },
   {
     $addFields: {
       tripTime: {
-        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+        $subtract: ["$stopTime", "$startTime"],
       },
     },
   },
   {
     $group: {
       _id: null,
-      duracaoMediaEmMinutos: { $avg: "$tripTime" },
+      avgTripTime: { $avg: "$tripTime" },
     },
   },
   {
     $project: {
       _id: false,
-      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+      duracaoMediaEmMinutos: { $ceil: { $divide: ["$avgTripTime", 60000] } },
     },
   },
 ]);
