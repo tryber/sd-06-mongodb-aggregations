7816c2fbbae8d7589bd684caa2023f82416a83c9
pa-volpin <paulo.volpin@gmail.com>
2021-01-29 00:20:19 -0300

    Corretion Task 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -3,5 +3,5 @@ db.trips.aggregate([
   { $group: { _id: "$dayOfWeek", total: { $sum: 1 } } },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } },
+  { $project: { _id: 0, diaDaSemana: "$_id", total: "$total" } },
 ]);
