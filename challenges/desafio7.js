04e2ab58c2cbcfc914350406a2a6418ebb380b60
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-01-29 14:53:33 -0300

    Desafio 6 refeito

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,18 +1,13 @@
 db.movies.aggregate(
   [
-    { $match: { awards: { $regex: /^Won/ }, type: "movie" } },
+    { $match: { languages: "English" } },
+    { $unwind: "$cast" },
     { $group: {
-      _id: "imdb.rating",
-      maior_rating: { $max: "$imdb.rating" },
-      menor_rating: { $min: "$imdb.rating" },
-      media_rating: { $avg: "$imdb.rating" },
-      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
-    } },
-    { $project: {
-      maior_rating: 1,
-      menor_rating: 1,
-      media_rating: { $round: ["$media_rating", 1] },
-      desvio_padrao: { $round: ["$desvio_padrao", 1] },
-    } },
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" } },
+    },
+    { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
+    { $sort: { numeroFilmes: -1, _id: -1 } },
   ],
 );
