599c7326761c0394b3e79a7fcc2f5bd33dd214bd
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-01-29 02:04:15 -0300

    req1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,5 +2,5 @@ db.movies.aggregate([{ $match: {
   "imdb.rating": { $gte: 7 },
   genres: { $nin: ["Crime", "Horror"] },
   rated: { $in: ["PG", "G"] },
-  languages: { $in: ["English", "Spanish"] },
-} }]).pretty();
+  languages: { $all: ["English", "Spanish"] },
+} }]);
