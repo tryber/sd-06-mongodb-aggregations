14f6ec064e9a4e9fe421c4e252804a5da35a3517
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-01-29 23:34:13 -0300

    lint

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,18 +1,18 @@
 db.air_routes.aggregate([
   { $match: {
-      airplane: { $in: ["747", "380"] },
-    } },
+    airplane: { $in: ["747", "380"] },
+  } },
   { $lookup: {
-      from: "air_alliances",
-      localField: "airline.name",
-      foreignField: "airlines",
-      as: "alliance",
-    } },
+    from: "air_alliances",
+    localField: "airline.name",
+    foreignField: "airlines",
+    as: "alliance",
+  } },
   { $unwind: "$alliance" },
   { $group: {
-      _id: "$alliance.name",
-      totalRotas: { $sum: 1 },
-    } },
+    _id: "$alliance.name",
+    totalRotas: { $sum: 1 },
+  } },
   { $sort: { totalRotas: -1 } },
   { $limit: 1 },
 ]);
