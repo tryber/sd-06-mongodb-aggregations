e3157c9d41ddcebc47fa4932c77ae6ac6c9688e2
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-01-30 17:57:11 -0300

    fix: lint issues 1 - 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,20 +1,20 @@
 db.movies.aggregate([
-    { $project: {
+  {
+    $project: {
       _id: 0,
       title_split: {
         $split: ["$title", " "],
       },
     },
+  },
+  {
+    $match: {
+      $expr: { $eq: [{ $size: "$title_split" }, 1] },
     },
-    {
-      $match: {
-        $expr: { $eq: [{ $size: "$title_split" }, 1] },
-      },
+  },
+  {
+    $sort: {
+      title_split: 1,
     },
-    {
-      $sort: {
-        title_split: 1,
-      },
-    },
-  ]);
-  
+  },
+]);
