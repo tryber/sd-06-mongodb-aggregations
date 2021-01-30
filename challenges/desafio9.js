76462587e811dba006df17d7966a4a0a62a4d019
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-30 17:24:50 -0300

    desafio 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -6,9 +6,10 @@ db.trips.aggregate([
     ],
   } },
   { $project: { _id: 0, birthYear: { $toInt: "$birthYear" } } },
-  { $project: {
-    _id: 0,
+  { $group: {
+    _id: null,
     maiorAnoNascimento: { $max: "$birthYear" },
     menorAnoNascimento: { $min: "$birthYear" },
   } },
+  { $project: { _id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1 } },
 ]);
