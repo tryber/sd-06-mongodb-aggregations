da7b3efb2d7b3baf3be08011e26c762a4ded1807
deko possas <andrepossaster@gmail.com>
2021-01-30 10:29:20 -0300

    atores, filmes e rating

---
+++ challenges/desafio7.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  { $match: { languages: English } },
+  { $unwind: "cast" },
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $um: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  } },
+  { $sort: {
+    numeroFilmes: -1,
+    _id: -1,
+  } },
+  { $project: {
+    mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    numeroFilmes: 1,
+  } },
+]);
