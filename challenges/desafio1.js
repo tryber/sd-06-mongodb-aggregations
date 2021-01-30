4dedd836aebb9513d7cd8fe0de098ecae094e13f
rafMewtwo <ponciano.raf@gmail.com>
2021-01-30 20:24:12 -0300

    desafio1.4

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
   { $match: {
     "imdb.rating": { $gte: 7 },
     genres: { $nin: ["Crimes, Horror"] },
-    rated: { $in: ["PG", G] },
+    rated: { $in: ["PG", "G"] },
     languages: { $all: ["English", "Spanish"] },
   } },
 ]);
