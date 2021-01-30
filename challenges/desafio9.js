493c21cfa40e6a96814128c06607af4c5942cc55
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-01-30 18:27:50 -0300

    reqs 9 ate 12

---
+++ challenges/desafio9.js
@@ -0,0 +1,16 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: true, $ne: "" } } },
+  { $addFields: {
+    anoNascimento: { $toInt: "$birthYear" },
+  } },
+  { $group: {
+    _id: "null",
+    maiorAnoNascimento: { $max: "$anoNascimento" },
+    menorAnoNascimento: { $min: "$anoNascimento" },
+  } },
+  { $project: {
+    _id: 0,
+    maiorAnoNascimento: "$maiorAnoNascimento",
+    menorAnoNascimento: "$menorAnoNascimento",
+  } },
+]);
