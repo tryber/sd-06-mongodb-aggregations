71050d9a8ae36eff525ba4d7934b80cfd64206a8
Daniel Moura <darthur.moura@gmail.com>
2021-01-30 16:21:42 -0300

    Correções: 1 - 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -3,12 +3,12 @@ db.movies.aggregate([
     $match: {
       "imdb.rating": { $gte: 7 },
       genres: { $nin: ["Crime", "Horror"] },
-      "imdb.rating": { $in: ["PG", "G"] },
+      rated: { $in: ["PG", "G"] },
       languages: { $all: ["English", "Spanish"] },
     },
   },
   {
-    $project: { _id: false, titulo: "$title", avaliado: "$imdb.rating", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" },
+    $project: { _id: false, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" },
   },
   {
     $sort: { ano: -1, notaIMDB: -1, titulo: 1 },
