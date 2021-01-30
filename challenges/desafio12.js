60e4b43003a61a292aad230c3b893023ce4b149e
Dilenio Enderle <dilenio@gmail.com>
2021-01-30 19:20:46 -0300

    feat: challenge 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,27 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      $expr: { $eq: ["$diaDaSemana", 5] },
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: { _id: 0, nomeEstacao: "$_id", total: "$count" },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
