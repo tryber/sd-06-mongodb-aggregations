109db9804a3fad61b664a6404a3e2b933250e47b
Leandro <leandro.feitoza8018@gmail.com>
2021-01-30 16:25:45 -0300

    desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $exists: true },
+    },
+  },
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
