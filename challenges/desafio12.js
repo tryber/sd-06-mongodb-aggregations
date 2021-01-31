1b6df56926882c23a9d04630575bbd31959a7699
rafMewtwo <ponciano.raf@gmail.com>
2021-01-31 19:13:19 -0300

    desafio12.2

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,7 +1,7 @@
 db.trips.aggregate([
   { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, 5] } } },
   { $group: { _id: "$startStationName", count: { $sum: 1 } } },
-  { $sort: { total: -1 } },
   { $project: { nomeEstacao: "$_id", total: "$count", _id: false } },
+  { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
