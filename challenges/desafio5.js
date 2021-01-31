f2cca66bf0b5342f2bac8622827f5cad75bd69bd
Lais Gurgel <laisgurgelf@gmail.com>
2021-01-31 19:44:50 -0300

    Fixing challenge 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,45 +1,29 @@
+const favorites = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
 db.movies.aggregate([
   {
     $match: {
-      countries: { $elemMatch: { $eq: "USA" } },
+      cast: { $in: favorites },
+      countries: { $all: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $exists: true },
     },
   },
-  {
-    $addFields: {
-      array_favs: {
-        $setIntersection: [
-          ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
-          "$cast",
-        ],
-      },
-    },
+  { $addFields: { num_favs: { $size:
+    { $setIntersection:
+      [favorites, "$cast"] } } },
   },
-  {
-    $addFields: {
-      num_favs: { $size: "$array_favs" },
-    },
+  { $sort:
+    { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
   },
-  {
-    $sort: {
-      num_favs: -1,
-      "tomatoes.viwer.rating": -1,
-      title: -1,
-    },
+  { $project:
+    { _id: 0, title: 1 },
   },
-  {
-    $skip: 24,
-  },
-  {
-    $limit: 15,
-  },
-  {
-    $project: {
-      _id: 0,
-      title: 1,
-      num_favs: 1,
-      array_favs: 1,
-    },
-  },
+  { $skip: 24 },
+  { $limit: 1 },
 ]);
