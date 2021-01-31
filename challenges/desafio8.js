0384acf29588727fdf5619d044aa64e36bfb49c5
vapatty6848 <vapatty@gmail.com>
2021-01-31 09:02:45 -0300

    desafio 8 - erros de lint- doublequote

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,32 +1,32 @@
 db.air_alliances.aggregate([
-  { $unwind: '$airlines' },
+  { $unwind: "$airlines" },
   {
     $lookup: {
-      from: 'air_routes',
-      let: { air: '$airlines' },
+      from: "air_routes",
+      let: { air: "$airlines" },
       pipeline: [
         {
           $match: {
-            $or: [{ airplane: '747' }, { airplane: '380' }],
-            $expr: { $eq: ['$airline.name', '$$air'] },
+            $or: [{ airplane: "747" }, { airplane: "380" }],
+            $expr: { $eq: ["$airline.name", "$$air"] },
           },
         },
       ],
-      as: 'nome_da_alianca',
+      as: "nome_da_alianca",
     },
   },
   {
     $addFields: {
-      rotas: { $size: '$nome_da_alianca' },
+      rotas: { $size: "$nome_da_alianca" },
     },
   },
   {
     $group: {
-      _id: '$name',
-      total_de_rotas: { $sum: '$rotas' },
+      _id: "$name",
+      total_de_rotas: { $sum: "$rotas" },
     },
   },
   { $sort: { total_de_rotas: -1 } },
-  { $project: { _id: 1, totalRotas: { $max: ['$total_de_rotas'] } } },
+  { $project: { _id: 1, totalRotas: { $max: ["$total_de_rotas"] } } },
   { $limit: 1 },
 ]);
