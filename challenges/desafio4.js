094e61cf8fb048743336ba015b37911ad43e8bd8
rafMewtwo <ponciano.raf@gmail.com>
2021-01-31 00:21:31 -0300

    desafio4.2

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,7 +1,7 @@
 db.movies.aggregate([
   { $addFields: { title_split: { $split: ["$title", " "] } } },
   { $match: { title_split: { $size: 1 } } },
-  { $sort: {title_split: 1 } },
+  { $sort: { title_split: 1 } },
   { $project: {
     _id: false,
     title_split: true,
