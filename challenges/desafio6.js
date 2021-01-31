54b08d0b4f0b04fd791edf306dbce3ffce5ab61c
Cássio Rubens <cassiorubensadv@gmail.com>
2021-01-31 13:19:56 -0300

    FIX: Desafio 06

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,8 +1,10 @@
 // Desafio 6
 // Vamos explorar mais operadores aritméticos!
-// Considerando todos os filmes que ganharam o Oscar pelo menos uma vez, calcule o maior valor, menor valor, média e o desvio padrão das avaliações (campo imdb.rating)
+// Considerando todos os filmes que ganharam o Oscar pelo menos uma vez, calcule o maior valor,
+// menor valor, média e o desvio padrão das avaliações (campo imdb.rating)
 // Para a média e o desvio padrão arredonde os valores para uma casa decimal utilizando o $round.
-// Dica: todos os filmes na coleção, que já ganharam um Oscar, começam com uma sequência de string parecida com essas abaixo, portanto $regex é um operador bem-vindo:
+// Dica: todos os filmes na coleção, que já ganharam um Oscar, começam com uma sequência de string
+// parecida com essas abaixo, portanto $regex é um operador bem-vindo:
 // Won 10 Oscars
 // Won 1 Oscar
 // Utilizem o $stdDevSamp para calcular o desvio padrão.
