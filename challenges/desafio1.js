b32988261a018d2c7fac66a8c1c8d0ec65c2a009
Isabela Rosa <isabelarosa954@gmail.com>
2021-01-31 15:36:26 -0300

    desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,13 @@
-// alteração inicial para commit
+db.movies.aggregate([
+  {
+    $match:
+    {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+]);
