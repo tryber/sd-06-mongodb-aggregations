578f223789f783149d6e475f7543f2b02e0c3a3a
Lais Gurgel <laisgurgelf@gmail.com>
2021-01-31 20:18:46 -0300

    Fixing challenge 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,30 +1,17 @@
-db.air_routes.aggregate([
-  {
-    $match: {
-      airplane: { $in: ["747", "380"] },
-    },
-  },
-  {
-    $lookup: {
-      from: "air_alliances",
-      localField: "airline.name",
-      foreignField: "airlines",
-      as: "alliance",
-    },
-  },
-  {
-    $unwind: "$alliance",
-  },
-  {
-    $group: {
-      _id: "$alliance.name",
-      totalRotas: { $sum: 1 },
-    },
-  },
-  {
-    $limit: 1,
-  },
-  {
-    $sort: { totalRotas: -1 },
-  },
+db.air_alliances.aggregate([
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "routes",
+  } },
+  { $unwind: "$routes" },
+  { $match: {
+    "routes.airplane": { $in: ["747", "380"] },
+  } },
+  { $group: {
+    _id: "$name", totalRotas: { $sum: 1 },
+  } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
 ]);
