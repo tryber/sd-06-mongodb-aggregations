d54f85ddb5cbe0d3216c03da4094aa53a6ac29f1
Cássio Rubens <cassiorubensadv@gmail.com>
2021-01-31 15:14:18 -0300

    FIX: Desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,5 +1,6 @@
 // Desafio 12
-// Usando a pipeline anterior que retorna o dia com mais viagens, determine qual estação tem o maior número de viagens nesse dia da semana.
+// Usando a pipeline anterior que retorna o dia com mais viagens,
+// determine qual estação tem o maior número de viagens nesse dia da semana.
 // Exiba apenas o nome da estação e o total de viagens.
 // Dica: Utilize o operador $dayOfWeek para extrair o dia da semana como um número de uma data.
 // O resultado da sua query deve ter o seguinte formato:
