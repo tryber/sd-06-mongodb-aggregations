525e9b534affb72c6003cc694f2ee97d1efe2088
Daniel Moura <darthur.moura@gmail.com>
2021-02-01 14:22:44 -0300

    Correção desafios 6 e 7

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,5 @@
 db.movies.aggregate([
-  { $match: { awards: { $regex: /^Won/ } } },
-  { $group: { maior_rating: { $max: "imdb.rating", menor_rating: { $min: "imdb.rating" }, media_rating: { $round: { $avg: "imdb.rating" } }, desvio_padrao: { $round: { $stdDevSamp: "imdb.rating" } } } } },
-  { project: { _id: 0, maior_rating: 1, menor_rating: 1, media_rating: 1, desvio_padrao: 1 } },
+  { $match: { awards: { $regex: /Won\s\d\sOscar/ } } },
+  { $group: { _id: 0, maior_rating: { $max: "$imdb.rating" }, menor_rating: { $min: "$imdb.rating" }, media_rating: { $avg: "$imdb.rating" }, desvio_padrao: { $stdDevSamp: "$imdb.rating" } } },
+  { $project: { _id: 0, maior_rating: 1, menor_rating: 1, media_rating: { $round: ["$media_rating", 1] }, desvio_padrao: { $round: ["$desvio_padrao", 1] } } },
 ]);
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,6 +1,7 @@
 db.movies.aggregate([
-  { $match: { language: { $elemMatch: { $eq: "English" } } } },
+  { $match: { languages: { $in: ["English"] } } },
   { $unwind: "$cast" },
-  { $group: { _id: "$cast", numeroFilmes: { $sum: 1 }, mediaIMDB: { $round: { $avg: "$imdb.rating" } } } },
+  { $group: { _id: "$cast", numeroFilmes: { $sum: 1 }, mediaIMDB: { $avg: "$imdb.rating" } } },
+  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
   { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);
