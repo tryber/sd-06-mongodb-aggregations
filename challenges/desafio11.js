1c4446b2511cddc9f7d309bb62a5d4bf43767e1b
kissyla <kissylamottapereira@gmail.com>
2021-02-01 15:34:05 -0300

    desafio 11 de novo

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -12,6 +12,13 @@ db.trips.aggregate([
       total: { $sum: 1 },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
   { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
