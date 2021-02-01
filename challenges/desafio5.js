54153c3d144eceb2340ecb97437688cb40406562
rafMewtwo <ponciano.raf@gmail.com>
2021-01-31 23:55:57 -0300

    desafio5.1

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -10,7 +10,7 @@ db.movies.aggregate([
   { $match: { countries: "USA", "tomatoes.viewer.rating": { $gte: 3 }, cast: { $exists: true } } },
   { $addFields: { num_favs: { $size: { $setIntersection: ["$cast", arrayActors] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
-  { $skip: 24 },
   { $project: { title: true, _id: false } },
+  { $skip: 24 },
   { $limit: 1 },
 ]);
