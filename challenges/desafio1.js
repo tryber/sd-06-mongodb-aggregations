6015da19d8c51d1728ecb98479f10d4677e791ae
ilanribeiro <ilanribeiro@gmail.com>
2021-01-31 22:17:54 -0300

    chg: modified syntax

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,10 +1,12 @@
-db.movies.aggregate()
-  .match({
+db.movies.aggregate([
+  { $match: {
     $and: [
       { "imdb.rating": { $gte: 7 } },
       { genres: { $not: { $in: ["Crime", "Horror"] } } },
       { $or: [{ rated: "PG" }, { rated: "G" }] },
       { languages: { $all: ["English", "Spanish"] } },
     ],
-  })
-  .limit(41);
+  } },
+  { $limit: 41 },
+
+]);
