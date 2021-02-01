c43be3714821aad480225cf9b82e03c7b81fa352
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-02-01 20:45:55 -0300

    req.05-ip

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,9 +1,31 @@
-// db.produtos.updateMany(
-//   { nome: { $ne: "McChicken" } },
-//   { $addToSet: { ingredientes: "ketchup" } },
-// );
+// cosnt actors = [ "Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney" ];
 
-// db.produtos.find(
-//   {},
-//   { nome: 1, ingredientes: 1, _id: 0 },
-// );
+// db.movies.aggregate([
+//   {
+//     $match: {
+//       countries: "USA",
+//       "tomatoes.viewer.rating": { $gte: 3 },
+//       cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
+//     },
+//   },
+//   {
+//     $addFields: {
+//       num_favs: {},
+//     },
+//   },
+//   {
+//     $sort: {
+//       num_favs: -1,
+//       "tomatoes.viewer.rating": -1,
+//       title: -1,
+//     },
+//   },
+//   { $skip: 24 },
+//   { $limit: 1 },
+//   {
+//     $project: {
+//       _id: 0,
+//       title: 1,
+//     },
+//   },
+// ]);
