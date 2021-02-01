6453d560ba340f67bcaa68431790cd8910f3c47c
rafMewtwo <ponciano.raf@gmail.com>
2021-02-01 01:01:36 -0300

    desafio14

---
+++ challenges/desafio14.js
@@ -0,0 +1,11 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60] } },
+    },
+  },
+  { $sort: { duracaoMedia: -1 } },
+  { $project: { bikeId: "$_id", duracaoMedia: { $ceil: "$duracaoMedia" }, _id: false } },
+  { $limit: 5 },
+]);
