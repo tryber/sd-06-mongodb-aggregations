c2462a846f091883802b6bcfb430c4005ee53620
Daniel Moura <darthur.moura@gmail.com>
2021-02-02 20:24:27 -0300

    Correção: desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,5 +1,5 @@
 db.trips.aggregate([
   { $group: { _id: "$usertype", mediaDuracao: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } } } },
-  { $project: { _id: 0, tipo: "$_id", mediaDuracao: { $round: ["$mediaDuracao", 2] } } },
-  { $sort: { mediaDuracao: 1 } },
+  { $project: { _id: 0, tipo: "$_id", duracaoMedia: { $round: ["$mediaDuracao", 2] } } },
+  { $sort: { duracaoMedia: 1 } },
 ]);
