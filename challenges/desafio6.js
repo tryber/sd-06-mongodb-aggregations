26e24b57f736dc17d41b733b7405f05ffa9f89f2
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-02-01 22:07:28 -0300

    6 e 7

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,25 @@
-db.movies.aggregate([]);
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /^won . osc/i},
+    }
+  },
+  {
+    $group: {
+      _id: "$title",
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    }
+  }
+]).pretty();
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,1 +1,31 @@
-db.movies.aggregate([]);
+db.movies.aggregate([
+  {
+    $match: {
+      languages: "English",
+      
+    }
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaRating: { $avg: "$imdb.rating" },
+    }
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaRating", 1] },
+    },
+  },
+]).pretty();
