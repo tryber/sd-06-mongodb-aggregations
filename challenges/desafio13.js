83490f72011d94c0aab580fbe4cf69ed1a5ca420
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-02-02 11:58:48 -0300

    13° batendo com o resultado esperado

---
+++ challenges/desafio13.js
@@ -0,0 +1,33 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      day: { $dayOfMonth: "$startTime" },
+      month: { $month: "$startTime" },
+      year: { $year: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      day: 10,
+      month: 3,
+      year: 2016,
+    },
+  },
+  {
+    $project: {
+      duracaoEmMinutos: { $divide: [{ $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000] }, 60] },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: { $avg: "$duracaoEmMinutos" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $round: "$duracaoMediaEmMinutos" },
+    },
+  },
+]).pretty();
