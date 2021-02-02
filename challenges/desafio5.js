574af72533a6c676c0320a019a5f166da185b5dd
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-02-02 15:34:20 -0300

    req.05, 06

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,31 +1,56 @@
-// cosnt actors = [ "Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney" ];
+// cosnt actors = [
+//   "Sandra Bullock",
+//   "Tom Hanks",
+//   "Julia Roberts",
+//   "Kevin Spacey",
+//   "George Clooney"
+// ];
 
-// db.movies.aggregate([
-//   {
-//     $match: {
-//       countries: "USA",
-//       "tomatoes.viewer.rating": { $gte: 3 },
-//       cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
-//     },
-//   },
-//   {
-//     $addFields: {
-//       num_favs: {},
-//     },
-//   },
-//   {
-//     $sort: {
-//       num_favs: -1,
-//       "tomatoes.viewer.rating": -1,
-//       title: -1,
-//     },
-//   },
-//   { $skip: 24 },
-//   { $limit: 1 },
-//   {
-//     $project: {
-//       _id: 0,
-//       title: 1,
-//     },
-//   },
-// ]);
+db.movies.aggregate([
+  {
+    $addFields: { favActors: [
+      "Sandra Bullock",
+      "Tom Hanks",
+      "Julia Roberts",
+      "Kevin Spacey",
+      "George Clooney",
+    ] },
+  },
+  {
+    $match: {
+      $and: [
+        { countries: "USA" },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+        { cast: { $in: [
+          "Sandra Bullock",
+          "Tom Hanks",
+          "Julia Roberts",
+          "Kevin Spacey",
+          "George Clooney",
+        ] } },
+      ],
+    },
+  },
+  {
+    $addFields: {
+      num_favs: { $size: {
+        $setIntersection: ["$favActors", "$cast"],
+      } },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
