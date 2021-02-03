0f2ee6cd4ecf9d8796ae2afe32f3c3dd789ce230
Paulo Frazão <paulo4187@gmail.com>
2021-02-03 11:09:31 -0300

    add files challenges 2 to 8

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -2,4 +2,9 @@ db.movies.aggregate([
   { $match: { $and: [{ "imdb.rating": { $gte: 7 } },
     { $and: [{ languages: "English" }, { languages: "Spanish" }] },
     { $or: [{ rated: "PG" }, { rated: "G" }] }, { $nor: [{ genres: "Crime" }, { genres: "Horror" }] }] } },
-]);
+  { $project: { _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year" } }, { $sort: { ano: -1, notaIMDB: -1 } }]);
