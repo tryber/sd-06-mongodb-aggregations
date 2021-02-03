0e3f2e1f1a496f922f8e18f764a73a0ef358e7f8
Anderson R <misterregisslv@gmail.com>
2021-02-03 12:40:46 -0300

    desafio 13 concluido

---
+++ challenges/desafio13.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lte: ISODate("2016-03-10T23:59:59Z"),
+      },
+    },
+  },
+  {
+    $set: {
+      tempo: {
+        $divide: [{
+          $abs: {
+            $subtract: ["$startTime", "$stopTime"],
+          },
+        }, 3600000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "tempo",
+      duracaoMediaEmMinutos: { $avg: { $ceil: { $multiply: ["$tempo", 60] } } },
+    },
+  },
+  {
+    $project: { duracaoMediaEmMinutos: { $round: ["$duracaoMediaEmMinutos", 0] }, _id: 0 },
+  },
+]);
