bf8825c218e866d392b85a7a4855d65b3c9a4f24
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-02-02 21:26:06 -0300

    req.07

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,9 +1,37 @@
-// db.produtos.updateMany(
-//   {},
-//   { $pull: { ingredientes: "cebola" } },
-// );
+// cosnt actors = [
+//   "Sandra Bullock",
+//   "Tom Hanks",
+//   "Julia Roberts",
+//   "Kevin Spacey",
+//   "George Clooney"
+// ];
 
-// db.produtos.find(
-//   {},
-//   { nome: 1, ingredientes: 1, _id: 0 },
-// );
+db.movies.aggregate([
+  {
+    $match: {
+      cast: { $exists: true },
+      languages: { $eq: "English" },
+    },
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+]);
