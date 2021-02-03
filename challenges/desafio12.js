de91e3032e9442d98ee64b8eeb7d6da86c991e4c
Daniel Moura <darthur.moura@gmail.com>
2021-02-03 20:19:29 -0300

    Correção 12 (?)

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,7 +2,7 @@ db.trips.aggregate([
   { $addFields: { diaDaSemana: { $dayOfWeek: "$startTime" } } },
   { $match: { diaDaSemana: { $eq: 5 } } },
   { $group: { _id: "$startStationName", total: { $sum: 1 } } },
-  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: { $toInt: "$total" } } },
   { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
