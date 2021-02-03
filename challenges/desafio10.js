fd4bf524d0224413e8e4cc8f37a1ccc5b9d95076
RenatoCostaDev <renatotrybe@gmail.com>
2021-02-03 15:14:35 -0300

    fix:Requirement-10.2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,14 +2,14 @@ db.trips.aggregate([
   {
     $group: {
       _id: "$usertype",
-      timer: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+      timeFly: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
     },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: { $round: [{ $divide: ["$timer", 3.6e+6] }, 2] },
+      duracaoMedia: { $round: [{ $divide: ["$timeFly", 3.6e+6] }, 2] },
     },
   },
   { $sort: { duracaoMedia: 1 } },
