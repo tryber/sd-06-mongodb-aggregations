13444645371db18755fce41c2c316059dde1377b
Alessandra <itsalematos@hotmail.com>
2021-02-03 13:10:33 -0400

    feat: 1, 2, 3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -7,4 +7,14 @@ db.movies.aggregate([
         languages: { $all: ["English", "Spanish"] },
       },
     },
+    {
+      $project: {
+        titulo: "$title",
+        avaliado: "$rated",
+        notaIMDB: "$imdb.rating",
+        votosIMDB: "$imdb.votes",
+        ano: "$year",
+        _id: 0,
+      },
+    },
   ]);
