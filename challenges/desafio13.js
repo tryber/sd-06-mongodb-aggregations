b7408c7f0da741dce8b0dc0b3298701259b82a78
Marcelo Pessini <marceloppessini@gmail.com>
2021-02-03 16:35:45 -0300

    Desafio13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,18 +1,15 @@
 db.trips.aggregate([
-  { $match: { startTime: { $eq: ISODate("2016-03-10") } } },
+  { $match: {
+    startTime: {
+      $gte: ISODate("2016-03-10"),
+      $lt: ISODate("2016-03-11") } } },
   { $group: {
     _id: null,
     duracaoMediaEmMinutos: {
       $avg: {
-        $divide: [
-          { $subtract: ["$stopTime", "$startTime"] },
-          60000,
-        ],
-      },
-    },
-  } },
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] } } } },
   { $project: {
     _id: 0,
-    duracaoMediaEmMinutos: { $floor: "$duracaoMediaEmMinutos" },
+    duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
   } },
 ]);
