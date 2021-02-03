d9060c1ba49e51f523c5d2b8e984508c93faa6a4
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-02-02 23:41:46 -0300

    req.10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,21 +1,30 @@
-// db.trips.aggregate([
-//   {
-//     $match: {
-//       birthYear: { $exists: true, $ne: "" },
-//     },
-//   },
-//   {
-//     $group: {
-//       _id: null,
-//       maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
-//       menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
-//     },
-//   },
-//   {
-//     $project: {
-//       _id: 0,
-//       maiorAnoNascimento: 1,
-//       menorAnoNascimento: 1,
-//     },
-//   },
-// ]);
+db.trips.aggregate([
+  // {
+  //   $match: {
+  //     $and: [
+  //       {$_id: { $exists: true, $ne: "" }},
+  //       {$usertype: { $exists: true, $ne: "" }},
+  //       {$startTime: { $exists: true, $ne: "" }},
+  //       {$stopTime: { $exists: true, $ne: "" }},
+  //     ]
+  //   },
+  // },
+  {
+    $group: {
+      _id: "$usertype",
+      avgTime: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60 * 60],
+        },
+      },
+    },
+  },
+  { $sort: { avgTime: 1 } },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$avgTime", 2] },
+    },
+  },
+]);
