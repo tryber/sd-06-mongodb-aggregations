1f9d92b77509e9ae1ff999e273bffa127fa78f09
leoCavachini <cavachini240@gmail.com>
2021-02-03 15:27:59 -0300

    desafio9

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,6 @@
 db.movies.aggregate([
   {
-    $match: { awards: { $regex: /Won/ } },
+    $match: { awards: { $regex: /Won\s[1-9]\sOscar/i } },
   },
   {
     $group: { _id: 0, maior_rating: { $max: "$imdb.rating" }, menor_rating: { $min: "$imdb.rating" }, media_rating: { $avg: "$imdb.rating" }, desvio_padrao: { $stdDevSamp: "$imdb.rating" } },
