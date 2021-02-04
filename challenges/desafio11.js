68edc55580d44cade6bf87e7c95df9e6f7ed8c3d
Pedro Duarte <ikbenpedro@gmail.com>
2021-02-04 13:16:39 -0300

    11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,12 +1,19 @@
 db.trips.aggregate([
   {
+    $match: {
+      startTime: {$exists:true }
+  }
+  {
     $group: {
-      diaDaSemana: { $dayOfWeek: "$startTime" },
+      _id: { $dayOfWeek: "$startTime" },
       total: { $sum: 1 },
     },
   },
   {
-    $project: { _id: 0, diaDaSemana: 1, total: 1 },
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total" },
   },
   {
     $sort: { total: -1 },
