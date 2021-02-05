19104a6ffa333f8c09dbd1c7752105164772a9cf
ricknois <rickssti@outlook.com>
2021-02-05 01:01:22 -0300

    :sparkles: Requisitos 1 a 9

---
+++ challenges/desafio1.js
@@ -0,0 +1,4 @@
+db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 },
+  genres: { $not: { $in: ["Crime", "Horror"] } },
+  rated: { $in: ["G", "PG"] },
+  languages: { $all: ["English", "Spanish"] } } }]);
