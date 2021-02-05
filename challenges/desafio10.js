e1f22d562447746c7087d3bf088de35721d0736b
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-02-05 10:51:33 -0300

    Fix lint desafio10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,13 +1,13 @@
 db.trips.aggregate([
-	{ $group:
-		{
-      _id: "$usertype",
-			duracaoMedia: { $avg:
-				{ $divide:
-					[{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
-        },
+{ $group:
+	{
+    _id: "$usertype",
+		duracaoMedia: { $avg:
+			{ $divide:
+				[{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
       },
     },
+  },
   },
   {
     $project: {
