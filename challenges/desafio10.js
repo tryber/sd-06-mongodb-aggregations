f4178864f3bea1f98300f43234c1315b537c1b88
Isabela Rosa <isabelarosa954@gmail.com>
2021-02-05 11:22:58 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide:
+            [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+]);
+
+/* PR do Matheus me ajudou novamente na parte de manipulação das horas.
+Não tinha me atentado que o retorno seria em segundos e teria que fazer a
+transformação com a divisão. */
