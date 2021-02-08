0bffd05d80d7400892d7f6a97d1703bb4321ea5b
PAULO LINS <paulolins100@gmail.com>
2021-02-08 16:51:31 -0300

    desafio12fix

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,16 +1,16 @@
 db.trips.aggregate([
   { $match: {
-      $expr: { $eq: [{
-      $dayOfWeek: "$startTime" }, 5] } },
+    $expr: { $eq: [{
+    $dayOfWeek: "$startTime" }, 5] } },
   },
   { $group: {
-      _id: "$startStationName",
-      total: { $sum: 1 } },
+    _id: "$startStationName",
+    total: { $sum: 1 } },
   },
   { $project: {
-      _id: 0,
-      nomeEstacao: "$_id",
-      total: "$total" },
+    _id: 0,
+    nomeEstacao: "$_id",
+    total: "$total" },
   },
   { $sort: { total: -1 } },
   { $limit: 1 },
