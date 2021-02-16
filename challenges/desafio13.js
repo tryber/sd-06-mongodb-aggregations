560d182c59e48f0810c4891bbae78866594de4ff
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-02-15 21:06:51 -0300

    Finish challenge 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $expr: {
+        $eq: [
+          {
+            $dateToString: { format: "%Y-%m-%d", date: "$startTime" }
+          },
+          "2016-03-10"
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
