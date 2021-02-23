82d55ad0bcaa8ea1ada3e9f1108ccf0847c3cf63
danilocouto1 <dansantosalfa@gmail.com>
2021-02-23 19:19:36 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      diaDaSemana: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$diaDaSemana",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
+
