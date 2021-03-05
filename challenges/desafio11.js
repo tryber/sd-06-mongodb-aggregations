87335a3cd89ca1ed8c44ca032338cc7829fb0e37
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-03-05 20:01:21 -0300

    fixing some questions

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,19 @@
-db.trips.aggregate([{ $addFields: { dayOfWeek: { $dayOfWeek: "$startTime" } } }, { $group: { _id: "$dayOfWeek", total: { $sum: true } } }, { $project: { _id: 0, diaDaSemana: "$_id", total: "$total" } }, { $sort: { total: -1 } }, { $limit: 1 }]);
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        $dayOfWeek: "$startTime",
+      },
+      total: { $sum: true },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
