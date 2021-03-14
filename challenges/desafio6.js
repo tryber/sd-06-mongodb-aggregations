2ba516d573a7a0f2389a7aef3134c4028bde9a7c
Thiago <asm.thiago@gmail.com>
2021-03-14 20:45:51 -0300

    Requisitos 6 a 10

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,27 +1,33 @@
-db.movies.aggregate(
-  [
-    {
-      $match: {
-        awards: { $regex: /Won (?:1(?:[0-9] Oscars| Oscar)|[2-9] Oscars)/ },
-      },
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /Won (?:1(?:[0-9] Oscars| Oscar)|[2-9] Oscars)/ },
     },
-    {
-      $group: {
-        _id: null,
-        maior_rating: { $max: "$imdb.rating" },
-        menor_rating: { $min: "$imdb.rating" },
-        media_rating: { $avg: "$imdb.rating" },
-        desvio_padrao: { $stdDevSamp: "$imdb.rating" },
-      },
+  },
+  {
+    $group: {
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+      _id: null,
     },
-    {
-      $project: {
-        _id: 0,
-        maior_rating: 1,
-        menor_rating: 1,
-        media_rating: { $round: ["$media_rating", 1] },
-        desvio_padrao: { $round: ["$desvio_padrao", 1] },
-      },
+  },
+  {
+    $set: {
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
-  ],
-);
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: 1,
+      desvio_padrao: 1,
+    },
+  },
+]);
+
+//Regex pego no pull request da aluna Carla Nakajuni da T5.
---
+++ challenges/desafio8.js
@@ -0,0 +1,30 @@
+db.air_alliances.aggregate([
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "nomeAirline",
+    },
+  },
+  {
+    $unwind: "$nomeAirline",
+  },
+  {
+    $match: {
+      "nomeAirline.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { totalRotas: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
